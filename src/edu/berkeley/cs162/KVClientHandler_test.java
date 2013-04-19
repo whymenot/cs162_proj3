85d1c60177dfb7f46e123533e6229d116289dd44
Jeff Crane <jeff_crane@berkeley.edu>
2013-04-18 18:56:11 -0700

    Adding tests

---
+++ src/edu/berkeley/cs162/KVClientHandler_test.java
@@ -0,0 +1,14 @@
+package edu.berkeley.cs162;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KVClientHandler_test {
+
+	@Test
+	public void KVClientHandlerTest() {
+		fail("Not yet implemented");
+	}
+
+}
